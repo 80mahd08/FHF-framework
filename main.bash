48c4e2cb54e88aad9754ad9b93b23039083959a4
80mahd08 <amarimahdi8@gmail.com>
2024-08-04 14:57:13 +0100

    compile every one file and add main.bash file

---
+++ main.bash
@@ -0,0 +1,5 @@
+sass src/utilities/base.scss dist/base.css
+sass src/utilities/colors.scss dist/colors.css
+sass src/utilities/layout.scss dist/layout.css
+sass src/utilities/normalize.scss dist/normalize.css
+sass src/utilities/typography.scss dist/typography.css
